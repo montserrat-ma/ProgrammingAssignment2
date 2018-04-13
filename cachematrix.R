d0687b1f76af21a058b288714dddaa42b0ef2f01
montserrat-ma <montserrat-ma@users.noreply.github.com>
2018-04-13 15:36:13 +0200

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: It creates a 'matrix' object X that can cache its inverse
+## cacheSolve: It computes the inverse of X returned by makeCacheMatrix.
+##             If the inverse of X has already been calculated, it is used
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Caching the inverse of a Matrix (we assume it is invertible)
+makeCacheMatrix <- function(X = matrix()) {
+    iX <- NULL
+    setmat <- function(Y) {
+     X <<- Y
+     iX <<- NULL
+    }
+    getmat <- function() X
+    setinvm <- function(solve) iX <<- solve
+    getinvm <- function() iX
+    list(setmat = setmat, getmat = getmat, setinvm = setinvm, getinvm = getinvm)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve: Return a matrix that is the inverse of 'X'
+cacheSolve <- function(X, ...) {
+    iX <- X$getinvm()
+    if(!is.null(iX)) {
+        message("getting cached data")
+        return(iX)# it ends here
+    }
+    data <- X$getmat()
+    iX <- solve(data, ...)
+    X$setinvm(iX)
+    iX
 }
+
+## To test it (you can check it deleting the #):
+
+#A<-matrix(sample(1:16),nrow=4)
+#if (det(A)!=0 ) {
+#    x<-makeCacheMatrix(A)
+#    cacheSolve(x)
+#} else {
+#    print("Non invertible matrix")
+#}
+#solve(A)
